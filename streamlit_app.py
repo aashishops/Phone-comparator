887ee87ce96eec667ef5f375b4d2faaa72d3e76c
Aashish Sekar <107553285+aashishops@users.noreply.github.com>
2023-11-30 18:52:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -152,7 +152,7 @@ def compare_phones():
 
 def add_footer():
   
-    st.footer("Made by [Aashish](https://aashish-sekar.netlify.app/) with 🖤")
+    st.markdown("Made by [Aashish](https://aashish-sekar.netlify.app/) with 🖤")
     
 # Main menu options
 menu_option = st.sidebar.selectbox("Select an option:", ["Home", "Filter by Antutu Score", "Filter by Battery Capacity", "Company-wise Comparison", "Compare Phones"])
